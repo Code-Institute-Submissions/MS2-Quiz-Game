10697b9bddfbd32d182e8f97e1de0e86a1d959ee
Federvgh <caffarofederico@gmail.com>
2021-02-08 21:02:15 +0000

    test

--- game.js
+++ game.js
@@ -1,24 +1,21 @@
 document.querySelector(".Get-Quote").addEventListener("click", GetAPI);
 
 function GetAPI(e) {
-  const xhttp = new XMLHttpRequest();
+  const data = null;
 
-  xhttp.open("GET", "http://numbersapi.com/random/trivia"), true;
+const xhttp = new XMLHttpRequest();
+xhttp.withCredentials = true;
 
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      document.querySelector("#question").innerHTML = this.responseText;
-    }
-  };
+xhttp.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		console.log("response",this.responseText);
+	}
+});
 
-  if (window.location.protocol.indexOf("https") == 0) {
-    var el = document.createElement("meta");
-    el.setAttribute("http-equiv", "Content-Security-Policy");
-    el.setAttribute("content", "upgrade-insecure-requests");
-    document.head.append(el);
-  }
+xhttp.open("GET", "https://my-json-server.typicode.com/Federvgh%20/MS2-Quiz-Game/numberFacts");
 
   xhttp.send();
 
   e.preventDefault();
+
 }
