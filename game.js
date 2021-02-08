50ce173d59a0343d3ac306837587a85f722cbc10
Federico <caffarofederico@gmail.com>
2021-02-08 13:45:07 +0000

    remove"fix HTTPS request issue" code-didnt fix it

--- game.js
+++ game.js
@@ -11,6 +11,13 @@ function GetAPI(e) {
     }
   };
 
+  if (window.location.protocol.indexOf("https") == 0) {
+    var el = document.createElement("meta");
+    el.setAttribute("http-equiv", "Content-Security-Policy");
+    el.setAttribute("content", "upgrade-insecure-requests");
+    document.head.append(el);
+  }
+
   xhttp.send();
 
   e.preventDefault();
